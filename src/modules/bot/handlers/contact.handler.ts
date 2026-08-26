09fc18a9e4ea31a0988afbbc05ebcfe8188d56f4
qb1ty <arsenbro0707@mail.ru>
2026-08-27 02:45:28 +0500

    feat(bot): implement contact handler and complete auth flow via grpc

--- src/modules/bot/handlers/contact.handler.ts
+++ src/modules/bot/handlers/contact.handler.ts
@@ -1,11 +1,40 @@
+import { TelegramCompleteRequest } from "@qb1tycinema/contracts/gen/auth"
+import { lastValueFrom } from "rxjs"
 import type { Telegraf } from "telegraf"
 
+import { authClient } from "@/infrastructure/grpc"
 import type { TelegrafContext } from "@/shared/interfaces"
 
 export function registerContactHandler(bot: Telegraf<TelegrafContext>) {
 	bot.on("contact", async ctx => {
 		const phone = ctx.message.contact.phone_number
 
-		console.log("Result: ", ctx.message.contact)
+		if (!ctx.chat.id || !ctx.session.id) {
+			return ctx.reply(
+				"Произошла ошибка. Пожалуйста, начните процесс через сайт"
+			)
+		}
+
+		const request: TelegramCompleteRequest = {
+			sessionId: ctx.session.id,
+			phone
+		}
+
+		const { sessionId } = await lastValueFrom(
+			authClient.telegramComplete(request)
+		)
+
+		await ctx.reply("Регистрация успешна завершена", {
+			reply_markup: {
+				inline_keyboard: [
+					[
+						{
+							text: "Вернутся на сайт",
+							url: `https://qb1tycinema.kz/auth/tg-finalize?sessionId=${sessionId}`
+						}
+					]
+				]
+			}
+		})
 	})
 }
